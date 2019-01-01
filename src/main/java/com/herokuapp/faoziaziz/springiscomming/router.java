085cee2cbe646b58416af4c17edde02149243866
azizfaozi <admin@labseni.com>
2019-01-01 15:30:09 +0700

    berhasil thymeleafnya

--- src/main/java/com/herokuapp/faoziaziz/springiscomming/router.java
+++ src/main/java/com/herokuapp/faoziaziz/springiscomming/router.java
@@ -1,13 +1,14 @@
 package com.herokuapp.faoziaziz.springiscomming;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class router {
 
+    /*
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -15,5 +16,11 @@ public class router {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    */
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
