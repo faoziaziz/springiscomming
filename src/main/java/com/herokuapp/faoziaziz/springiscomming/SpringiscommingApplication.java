8dd2e7b25121bfcbfbffbec54376efa4352fa382
azizfaozi <admin@labseni.com>
2019-01-01 15:08:57 +0700

    first commit

---
+++ src/main/java/com/herokuapp/faoziaziz/springiscomming/SpringiscommingApplication.java
@@ -0,0 +1,14 @@
+package com.herokuapp.faoziaziz.springiscomming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringiscommingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringiscommingApplication.class, args);
+	}
+
+}
+
